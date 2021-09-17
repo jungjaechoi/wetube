9f1925671436e57fd580722f36ca7cb21efe2234
jungjaechoi <www898975@gmail.com>
2021-09-17 16:02:11 +0900

    a

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,14 +1,35 @@
-const fakeUser = {
-    username: "Nicolas",
-    loggedIn: false
-};
+let videos = [
+  {
+    title: "First video",
+    comments: 2,
+    views : 50,
+    id: 1
+  },
+  {
+    title: "Second video",
+    comments: 12,
+    views : 106,
+    id: 2
+  }, {
+    title: "Third video",
+    comments: 5,
+    views : 62,
+    id: 3
+  }
+];
 
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
-export const see = (req, res) => res.render("watch");
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) =>{
+  const {id} = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", {pageTitle: `Watching ${video.title}`});
+}
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
 };
-//export 로 수출
+//export 로 수출 
