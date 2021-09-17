d44e082085627d04628120dd809276ce968749ec
root <root@DESKTOP-H8U9OVL.localdomain>
2021-09-17 22:32:41 +0900

    a

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,7 +1,7 @@
 export const see = (req,res) => res.render("watch");
 
-export const trending = (req,res) => res.render("home");
-export const edit = (req,res) => res.send('edit');
+export const trending = (req,res) => res.render("home", { pageTitle: "Home" }); // pageTitle 변수 넘겨줌
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send('Search');
 export const upload = (req,res) => res.send('Upload');
 export const deleteVideo = (req,res) => res.send('Delete video');
