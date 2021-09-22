a8abce1aa1d4d8fa2610786b770c8614caa6a6c5
ndb796 <arnold0330@naver.com>
2021-09-23 02:28:07 +0900

    db 만들기 전

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,57 +1,32 @@
-let videos = [
-  {
-    title: "First video",
-    comments: 2,
-    views : 1,
-    id: 1
-  },
-  {
-    title: "Second video",
-    comments: 12,
-    views : 106,
-    id: 2
-  }, {
-    title: "Third video",
-    comments: 5,
-    views : 62,
-    id: 3
-  }
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {});
+  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I finish first");
 };
-export const watch = (req, res) =>{
-  const {id} = req.params; // ES6 문법 const id = req.rarams.id와 동일
-  const video = videos[id - 1];
-  return res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+export const watch = (req, res) => {
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
-  const {id} = req.params; 
-  const video = videos[id - 1];
-  return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
-  const {id} = req.params; // url로부터 params해서 id 가져오고
-  const { title } = req.body; // body로 입력값 받아옴
-  videos[id - 1].title = req.body.title;
+  const { id } = req.params;
+  const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
-//export 로 수출 
-//videos[id-1]에 넘길 때 req.body로 넘기면 안되고 title로 넘겨야함
-// => req.body는 배열이므로 req.body.title이 맞는거였음
-
-export const getUpload = (req,res) => {
-    return res.render("upload", {pageTitle: `Upload Video`});
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
-
-export const postUpload = (req,res) => {
-    const newVideo = {
-        title: req.body.title,
-        comments: 0,
-        views: 0,
-        id: videos.length + 1
-    };
-    videos.push(newVideo);
-    return res.redirect("/");
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;//암묵적으로 4000사용
-
 const app = express();
 const logger = morgan("dev"); //morgan에서 dev툴 사용, middleware 모듈
 
@@ -15,7 +13,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleLsn = () => 
-console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(4000, handleLsn);
+export default app;
